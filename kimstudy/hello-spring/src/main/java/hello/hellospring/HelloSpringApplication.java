d7c03a620156fe3323f41f40b99d0eeaf0c1e4c0
승엽 <tmdduqflfl@naver.com>
2024-03-02 10:00:00 +0900

    study

---
+++ kimstudy/hello-spring/src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -0,0 +1,14 @@
+package hello.hellospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringApplication.class, args);
+	}
+
+}
+// 대상은 스프링을 처음 접하는 개발자
